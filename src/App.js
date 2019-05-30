e00811b314a5b356e837f7df7606cf08dc594474
Stephen White <smw@Stephens-MacBook-Pro.local>
2019-05-29 21:14:06 -0400

    feat: basic react app setup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button from '@material-ui/core/Button';
+// import { Button as SButton } from '@seman/semantic-ui-react'
+
+// import logo from './logo.svg';
+import './App.scss';
+import CButton from './Components/CButton'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <CButton />
+        <Button variant="contained" color="primary" onClick={() => console.log('test')}>
+          Hello World
+        </Button>
       </div>
     );
+    // return (
+    //   <div className="App">
+    //     <header className="App-header">
+    //       <img src={logo} className="App-logo" alt="logo" />
+    //       <p> 
+    //         Edit <code>src/App.js</code> and save to reload.
+    //       </p>
+    //       <a
+    //         className="App-link"
+    //         href="https://reactjs.org"
+    //         target="_blank"
+    //         rel="noopener noreferrer"
+    //       >
+    //         Learn React
+    //       </a>
+    //     </header>
+    //   </div>
+    // );
   }
 }
 
